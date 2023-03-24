f5e29b5fe6b22ef51711aac7bb684669e82d76f5
Carter <me@fyko.net>
2023-03-24 15:53:39 -0600

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# yarn-plugin-git-hooks
+
+Yarn Berry plugin for easy Git Hooks.
+
+## Installation
+
+```sh
+$ yarn plugin import https://raw.githubusercontent.com/trufflehq/yarn-plugin-git-hooks/main/bundles/%40yarnpkg/plugin-git-hooks.js
+```
+
+## Usage
+
+1. Create your Git Hooks. We recommend `.github/hooks`.
+
+> **Warning**:  
+> Make sure they are are executable (`chmod +x ./commit-msg`)!
+>
+> ```ansi
+> hint: The '.github/hooks/commit-msg' hook was ignored because it's not set as executable.
+> ```
+
+2. Specify the path to your Git Hooks directory in `.yarnrc.yml`:
+
+```yaml
+gitHooksPath: .github/hooks
+```
+
+Now, every time you run `yarn` (once packages are installed), your hooks will be configured.
+
+> **Info**:  
+> You can check the configuration succeeded with `git config --get core.hookspath`.
---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import process from 'node:process';
+import { type Plugin, type Project, SettingsType, execUtils } from '@yarnpkg/core';
+import ci from 'ci-info';
+
+declare module '@yarnpkg/core' {
+  // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+  interface ConfigurationValueMap {
+    gitHooksPath: string | null;
+  }
+}
+
+const plugin: Plugin = {
+  configuration: {
+    gitHooksPath: {
+      description: 'Path to git hooks directory (recommended: .github/hooks)',
+      type: SettingsType.STRING,
+      default: null,
+    },
+  },
+  hooks: {
+    afterAllInstalled: async (project: Project) => {
+      const path = project.configuration.get('gitHooksPath');
+      const isDlx = Boolean(project.cwd?.endsWith(`dlx-${process.pid}`));
+
+      if (path && !ci.isCI && !isDlx) {
+        return execUtils.pipevp('git', ['config', 'core.hooksPath', path], {
+          cwd: project.cwd,
+          strict: true,
+          stdin: process.stdin,
+          stdout: process.stdout,
+          stderr: process.stderr,
+        });
+      }
+    },
+  },
+};
+
+export default plugin;
